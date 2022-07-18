0b38bc631b4e6679a3efe0833338dde00d7c2632
aorus <lmunoz-q@student.42.fr>
2022-07-19 01:46:54 +0200

    ex01

--- ex01/exec.py
+++ ex01/exec.py
@@ -1,9 +1,3 @@
 import sys
 
-
-def swap(string):
-    return string[-1:] + string[1:-1] + string[:1]
-
-
-if len(sys.argv) > 1:
-    print(swap(sys.argv[1:]))
+print(' '.join(sys.argv[1:])[::-1].swapcase())
