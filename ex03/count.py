f2ec496c82ae05b2e0a42355b426b6fe18be3311
Leonardo MUNOZ QUINTERO <lmunoz-q@e2r6p8.clusters.42paris.fr>
2022-07-30 18:22:40 +0200

    ok

--- ex03/count.py
+++ ex03/count.py
@@ -1,7 +1,20 @@
+# This script has to be executed in a python console according to the subject
+
 import string
+import sys
 
 
-def text_analyzer(text):
+def text_analyzer(*args):
+    """
+    This function counts the number of characters, upper characters, lower characters,
+    punctuation characters and spaces in a given text.
+    """
+    if len(args) > 1:
+        sys.exit("ERROR")
+    elif len(args) == 0:
+        text = str(input("What is the text to analyze ?\n"))
+    else:
+        text = args[0]
     char = len(text)
     upper = 0
     lower = 0
@@ -13,8 +26,8 @@ def text_analyzer(text):
             upper += 1
         elif character.islower():
             lower += 1
-        elif character in ("!", ",", "\'", ";", "\"", ".", "-", "?"):
+        elif character in string.punctuation:
             punt += 1
-        elif character in " ":
+        elif character in string.whitespace:
             space += 1
     print(ret.format(char, upper, lower, punt, space))
